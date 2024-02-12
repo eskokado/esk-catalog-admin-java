825f20412244db6a15b0d50222d53ed38473aa8a
eskokado <eskokado@gmail.com>
2024-02-12 10:59:44 -0300

    feat: create interface CategoryGateway

---
+++ domain/src/main/java/com/eskcti/catalog/admin/domain/category/CategoryGateway.java
@@ -0,0 +1,22 @@
+package com.eskcti.catalog.admin.domain.category;
+
+import com.eskcti.catalog.admin.domain.pagination.Pagination;
+import com.eskcti.catalog.admin.domain.pagination.SearchQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryGateway {
+
+    Category create(Category aCategory);
+
+    void deleteById(CategoryID anId);
+
+    Optional<Category> findById(CategoryID anId);
+
+    Category update(Category aCategory);
+
+    Pagination<Category> findAll(SearchQuery aQuery);
+
+    List<CategoryID> existsByIds(Iterable<CategoryID> ids);
+}
