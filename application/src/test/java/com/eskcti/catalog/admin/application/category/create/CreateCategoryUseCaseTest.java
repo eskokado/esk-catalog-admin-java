43adda42038be219a1da892df62045d866d2ba03
eskokado <eskokado@gmail.com>
2024-02-13 09:36:46 -0300

    test: given an invalid name when calls create category should return DomainException

--- application/src/test/java/com/eskcti/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
+++ application/src/test/java/com/eskcti/catalog/admin/application/category/create/CreateCategoryUseCaseTest.java
@@ -2,15 +2,28 @@ package com.eskcti.catalog.admin.application.category.create;
 
 import com.eskcti.catalog.admin.domain.category.Category;
 import com.eskcti.catalog.admin.domain.category.CategoryGateway;
+import com.eskcti.catalog.admin.domain.exceptions.DomainException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class CreateCategoryUseCaseTest {
+
+    @InjectMocks
+    private DefaultCreateCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
     @Test
     public void givenAValidCommand_whenCallsCreateCategory_shouldReturnCategoryId() {
         final var expectedName = "Filmes";
@@ -18,13 +31,13 @@ public class CreateCategoryUseCaseTest {
         final var expectedIsActive = true;
         final var expectedCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
 
-        final var aCommando = CreateCategoryCommand.with(expectedName, expectedDescription, expectedIsActive);
+        final var aCommando =
+                CreateCategoryCommand.with(
+                        expectedName, expectedDescription, expectedIsActive
+                );
 
-        final CategoryGateway categoryGateway = mock(CategoryGateway.class);
         when(categoryGateway.create(any()))
                 .thenAnswer(returnsFirstArg());
-
-        final var useCase = new DefaultCreateCategoryUseCase(categoryGateway);
 
         final var actualOutput = useCase.execute(aCommando);
 
@@ -44,5 +57,27 @@ public class CreateCategoryUseCaseTest {
                         }
                 )
         );
+    }
+
+    @Test
+    public void givenAnInvalidName_whenCallsCreateCategory_thenShouldReturnDomainException() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var aCommando =
+                CreateCategoryCommand.with(
+                        expectedName, expectedDescription, expectedIsActive
+                );
+
+        final var actualException = assertThrows(
+                DomainException.class, () -> useCase.execute(aCommando)
+        );
+
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+
+        verify(categoryGateway, times(0)).create(any());
     }
 }
