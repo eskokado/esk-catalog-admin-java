ed1c98f5c9a58c1b4b3c6242a8079dbf08641bbb
eskokado <eskokado@gmail.com>
2024-02-13 12:22:37 -0300

    feat: working with either and notification pattern in the create category use case

--- application/src/main/java/com/eskcti/catalog/admin/application/category/create/CreateCategoryUseCase.java
+++ application/src/main/java/com/eskcti/catalog/admin/application/category/create/CreateCategoryUseCase.java
@@ -1,7 +1,9 @@
 package com.eskcti.catalog.admin.application.category.create;
 
 import com.eskcti.catalog.admin.application.UseCase;
+import com.eskcti.catalog.admin.domain.validation.handler.Notification;
+import io.vavr.control.Either;
 
-public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, CreateCategoryOutput> {
+public abstract class CreateCategoryUseCase extends UseCase<CreateCategoryCommand, Either<Notification, CreateCategoryOutput>> {
 
 }
