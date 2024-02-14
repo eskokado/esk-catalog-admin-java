a5543aa215d15757347ffc3ce192c94a24b68749
eskokado <eskokado@gmail.com>
2024-02-14 12:08:49 -0300

    test: given a  valid id when calls delete category then should returns be ok

---
+++ application/src/test/java/com/eskcti/catalog/admin/application/category/delete/DeleteCategoryUseCaseTest.java
@@ -0,0 +1,54 @@
+package com.eskcti.catalog.admin.application.category.delete;
+
+import com.eskcti.catalog.admin.application.category.update.DefaultUpdateCategoryUseCase;
+import com.eskcti.catalog.admin.domain.category.Category;
+import com.eskcti.catalog.admin.domain.category.CategoryGateway;
+import com.eskcti.catalog.admin.domain.category.CategoryID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCategoryUseCaseTest {
+    @InjectMocks
+    private DefaultDeleteCategoryUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_thenShouldReturnsBeOk() {
+        final var aCategory = Category.newCategory("Filmes", "A categoria mais assistidas", true);
+        final var expectedId = aCategory.getId();
+
+        doNothing()
+                .when(categoryGateway)
+                .deleteById(eq(expectedId));
+
+        assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(eq(expectedId));
+    }
+
+
+}
