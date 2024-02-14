f09f45ff459b0b01ea2cdfaf1f5321f90c424d29
eskokado <eskokado@gmail.com>
2024-02-14 15:52:03 -0300

    test: given a valid query when calls list categories then should returns categories

---
+++ application/src/test/java/com/eskcti/catalog/admin/application/category/retrieve/list/ListCategoriesUseCaseTest.java
@@ -0,0 +1,66 @@
+package com.eskcti.catalog.admin.application.category.retrieve.list;
+
+import com.eskcti.catalog.admin.domain.category.Category;
+import com.eskcti.catalog.admin.domain.category.CategoryGateway;
+import com.eskcti.catalog.admin.domain.pagination.Pagination;
+import com.eskcti.catalog.admin.domain.pagination.SearchQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class ListCategoriesUseCaseTest {
+    @InjectMocks
+    private DefaultListCategoriesUseCase useCase;
+
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp() {
+        reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidQuery_whenCallsListCategories_thenShouldReturnsCategories() {
+        final var categories = List.of(
+                Category.newCategory("Filmes", null, true),
+                Category.newCategory("Series", null, true)
+        );
+
+        final var expectedPage = 0;
+        final var expectedPerPage = 0;
+        final var expectedTerms = "";
+        final var expectedSort = "createdAt";
+        final var expectedDirection = "asc";
+
+        final var aQuery =
+                new SearchQuery(expectedPage, expectedPerPage, expectedTerms, expectedSort, expectedDirection);
+
+        final var expectedPagination =
+                new Pagination<>(expectedPage, expectedPerPage, categories.size(), categories);
+
+        final var expectedItemsCount = 2;
+        final var expectedResult = expectedPagination.map(CategoryListOutput::from);
+
+        when(categoryGateway.findAll(eq(aQuery)))
+                .thenReturn(expectedPagination);
+
+        final var actualResult = useCase.execute(aQuery);
+
+        assertEquals(expectedItemsCount, actualResult.items().size());
+        assertEquals(expectedResult, actualResult);
+        assertEquals(expectedPage, actualResult.currentPage());
+        assertEquals(expectedPerPage, actualResult.perPage());
+        assertEquals(categories.size(), actualResult.total());
+    }
+}
