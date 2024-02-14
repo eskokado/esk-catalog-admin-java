ba4af6dcd225cf1f5accdaec2447223f43f2fdac
eskokado <eskokado@gmail.com>
2024-02-14 13:52:59 -0300

    test: given a valid id when calls get category then should return category

---
+++ application/src/test/java/com/eskcti/catalog/admin/application/category/retrieve/get/GetCategoryByIdUseCaseTest.java
@@ -0,0 +1,54 @@
+package com.eskcti.catalog.admin.application.category.retrieve.get;
+
+import com.eskcti.catalog.admin.domain.category.Category;
+import com.eskcti.catalog.admin.domain.category.CategoryGateway;
+import com.eskcti.catalog.admin.domain.category.CategoryID;
+import com.eskcti.catalog.admin.domain.exceptions.DomainException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class GetCategoryByIdUseCaseTest {
+    @InjectMocks
+    private DefaultGetCategoryByIdUseCase useCase;
+    @Mock
+    private CategoryGateway categoryGateway;
+
+    @BeforeEach
+    void cleanUp(){
+        reset(categoryGateway);
+    }
+
+    @Test
+    public void givenAValidId_whenCallsGetCategory_thenShouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var expectedId = aCategory.getId();
+
+        when(categoryGateway.findById(eq(expectedId)))
+                .thenReturn(Optional.of(Category.with(aCategory)));
+
+        final var actualCategory = useCase.execute(expectedId.getValue());
+
+        assertEquals(expectedId, actualCategory.id());
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(aCategory.getCreatedAt(), actualCategory.createdAt());
+        assertEquals(aCategory.getUpdatedAt(), actualCategory.updatedAt());
+        assertEquals(aCategory.getDeletedAt(), actualCategory.deletedAt());
+    }
+}
