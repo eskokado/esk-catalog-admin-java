81b73e2515dc01b9917a056aba5b6ba504cab0c0
eskokado <eskokado@gmail.com>
2024-02-15 15:05:30 -0300

    test: create annotation MySQLGatewayTest and impl

---
+++ infrastructure/src/test/java/com/eskcti/catalog/admin/infrastructure/category/CategoryMySQLGatewayTest.java
@@ -0,0 +1,21 @@
+package com.eskcti.catalog.admin.infrastructure.category;
+
+import com.eskcti.catalog.admin.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@MySQLGatewayTest
+public class CategoryMySQLGatewayTest {
+    @Autowired
+    private CategoryMySQLGateway categoryGateway;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    public void testInjectedDependencies() {
+        Assertions.assertNotNull(categoryGateway);
+        Assertions.assertNotNull(categoryRepository);
+    }
+}
