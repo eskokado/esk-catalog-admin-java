56f95eb5e293fe9810bd13638cfc4c030f1f087f
eskokado <eskokado@gmail.com>
2024-02-17 05:50:05 -0300

    feat: given a pre persisted category and valid category id when calls find by id should return a category

--- infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/category/CategoryMySQLGateway.java
@@ -36,7 +36,9 @@ public class CategoryMySQLGateway implements CategoryGateway {
 
     @Override
     public Optional<Category> findById(CategoryID anId) {
-        return Optional.empty();
+
+        return this.repository.findById(anId.getValue())
+                .map(CategoryJpaEntity::toAggregate);
     }
 
     @Override
