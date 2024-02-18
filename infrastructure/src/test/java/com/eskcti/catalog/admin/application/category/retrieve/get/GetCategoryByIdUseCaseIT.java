8b6ca69c0f9e808cba4d001ebf2bb25d2f6d617d
eskokado <eskokado@gmail.com>
2024-02-18 09:06:24 -0300

    test: given a valid id when calls get category then should return category

---
+++ infrastructure/src/test/java/com/eskcti/catalog/admin/application/category/retrieve/get/GetCategoryByIdUseCaseIT.java
@@ -0,0 +1,64 @@
+package com.eskcti.catalog.admin.application.category.retrieve.get;
+
+import com.eskcti.catalog.admin.IntegrationTest;
+import com.eskcti.catalog.admin.domain.category.Category;
+import com.eskcti.catalog.admin.domain.category.CategoryGateway;
+import com.eskcti.catalog.admin.infrastructure.category.persistence.CategoryJpaEntity;
+import com.eskcti.catalog.admin.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.ArgumentMatchers.eq;
+
+@IntegrationTest
+public class GetCategoryByIdUseCaseIT {
+    @Autowired
+    private GetCatetoryByIdUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidId_whenCallsGetCategory_thenShouldReturnCategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var aCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var expectedId = aCategory.getId();
+
+        assertEquals(0, categoryRepository.count());
+
+        save(aCategory);
+
+        assertEquals(1, categoryRepository.count());
+
+        final var actualCategory = useCase.execute(expectedId.getValue());
+
+        assertEquals(expectedId, actualCategory.id());
+        assertEquals(expectedName, actualCategory.name());
+        assertEquals(expectedDescription, actualCategory.description());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertEquals(aCategory.getCreatedAt().truncatedTo(ChronoUnit.SECONDS), actualCategory.createdAt().truncatedTo(ChronoUnit.SECONDS));
+        assertEquals(aCategory.getUpdatedAt().truncatedTo(ChronoUnit.SECONDS), actualCategory.updatedAt().truncatedTo(ChronoUnit.SECONDS));
+        assertNull(actualCategory.deletedAt());
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
