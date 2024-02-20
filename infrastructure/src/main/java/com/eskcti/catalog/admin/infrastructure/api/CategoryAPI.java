d44d21550f9be228d66035e9c199c21cd1aea199
eskokado <eskokado@gmail.com>
2024-02-20 09:18:25 -0300

    feat: given an invalid id when call get category then should return not found

--- infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/api/CategoryAPI.java
+++ infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/api/CategoryAPI.java
@@ -1,6 +1,7 @@
 package com.eskcti.catalog.admin.infrastructure.api;
 
 import com.eskcti.catalog.admin.domain.pagination.Pagination;
+import com.eskcti.catalog.admin.infrastructure.category.models.CategoryResponse;
 import com.eskcti.catalog.admin.infrastructure.category.models.CreateCategoryRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -8,10 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping(value = "categories")
 @Tag(name = "Categories")
@@ -43,4 +41,16 @@ public interface CategoryAPI {
             @RequestParam(name = "direction", required = false, defaultValue = "asc") final int direction
     );
 
+    @GetMapping(
+            value = "{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @Operation(summary = "Get a category by it's identifier")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Category retrieve successfully"),
+            @ApiResponse(responseCode = "404", description = "Category was not found"),
+            @ApiResponse(responseCode = "500", description = "An internal server error was throw")
+    })
+    CategoryResponse getById(@PathVariable(name = "id")  String id);
 }
--- infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/api/controllers/GlobalExceptionHandler.java
+++ infrastructure/src/main/java/com/eskcti/catalog/admin/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -2,7 +2,9 @@ package com.eskcti.catalog.admin.infrastructure.api.controllers;
 
 
 import com.eskcti.catalog.admin.domain.exceptions.DomainException;
+import com.eskcti.catalog.admin.domain.exceptions.NotFoundException;
 import com.eskcti.catalog.admin.domain.validation.Error;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,6 +22,16 @@ public class GlobalExceptionHandler {
             final HttpServletResponse res
     ) {
         return ResponseEntity.unprocessableEntity()
+                .body(ApiError.from(ex));
+    }
+    @ExceptionHandler(value = NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(
+            final DomainException ex,
+            final HttpServletRequest req,
+            final HttpServletResponse res
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(ApiError.from(ex));
     }
 
